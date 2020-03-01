842096fa24bfb8b46427d3c245cb0bd5a86a31e3
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-03-01 13:34:50 -0600

    Refacter to use params and format

--- lambdaCode.py
+++ lambdaCode.py
@@ -5,39 +5,39 @@ from pymongo import MongoClient
 import os
 
 def on_trigger(event, context):
-    connectionString = os.environ["MONGO_URL"]
+    #connectionString = os.environ["MONGO_URL"]
 
-    list = ['BTC', 'ETH',
+    currencies = ['BTC', 'ETH',
             'LTC', 'DAI',
             'EOS', 'XLM',
             'BAT', 'XRP',
             'LINK', 'DASH',
             'XTZ', 'ZEC', 'REP']
-    length = len(list)
+    num_currencies = len(currencies)
 
     client = MongoClient("mongodb+srv://emilys:UIUCchatbot@cluster0-ar3em.mongodb.net/test?retryWrites=true&w=majority")
     db = client["cryptocurrencyChatbot"]
 
     # Iterating the index 
-    # same as 'for i in range(len(list))' 
-    for i in range(length):
-        mycol = db[list[i]]
+    # same as 'for i in range(len(currencies))' 
+    for currency in currencies:
+        mycol = db[currency]
         #time.sleep(10)
         
         #Price of crypto
+        # timestamp is 12/02/2019 @ 2:30pm (UTC)
         timestamp = datetime.datetime.fromtimestamp(1575297000).isoformat()
-        price = str(float(requests.get("https://api.coinbase.com/v2/prices/" + list[i] + "-USD/buy").json()["data"]["amount"]))
+        price = str(float(requests.get("https://api.coinbase.com/v2/prices/{}-USD/buy".format(currency))
+            .json()["data"]["amount"]))
         
-
-        
-        url = "https://api.cryptowat.ch/markets/kraken/" + list[i] + "usd/summary"
+        url = "https://api.cryptowat.ch/markets/kraken/usd/summary".format(currency)
         response = requests.request("GET", url)
         
         #Absolute change in crypto
         change = str(response.json()["result"]["price"]["change"]["absolute"])
 
         #Percent change in crypto
-        #print("Percent change for " + list[i] + "(24hrs): " +
+        #print("Percent change for " + currencies[i] + "(24hrs): " +
         #          str(response.json()["result"]["price"]["change"]["percent"]))
         
         #High for crypto
@@ -51,15 +51,16 @@ def on_trigger(event, context):
         
         #MONGODB
         if(False):
-            url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=" + list[i] + "&market=USD&apikey=729WZLMQLLZMCYJF"  
-            response = requests.request("GET", url)
+            payload = {"function": "DIGITAL_CURRENCY_DAILY", "symbol": currency, "market": "USD", "apikey": "729WZLMQLLZMCYJF"}
+            # url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=" + currency + "&market=USD&apikey=729WZLMQLLZMCYJF"  
+            response = requests.request("GET", url, params=payload)
             
             try:
 
                 if (response.status_code == 200):
                 
                     #The timestamp is for yesterday (Some of the cryptos are not yet updated - easier to pull data for day before)
-                    timeStamp = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d');
+                    timeStamp = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d')
                     
                     #Open for crypto
                     print(str(response.json()["Time Series (Digital Currency Daily)"][f"{datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d')}"]["1a. open (USD)"]))
@@ -80,5 +81,5 @@ def on_trigger(event, context):
                 
             print("___________________________________")
 
-if __name__ == "__main":
-    ontrigger(None, None)
+if __name__ == "__main__":
+    on_trigger(None, None)
