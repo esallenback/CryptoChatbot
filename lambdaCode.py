99bafee541df7b4482336d695503a51f7584a1e1
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-03-15 20:10:26 -0500

    Add nomics API call; fix Kraken call

--- lambdaCode.py
+++ lambdaCode.py
@@ -8,26 +8,28 @@ from config import Config
 class CurrencyData():
     def __init__(self, currency):
         self.currency = currency
-        self.timestamp = datetime.datetime.now()
-        self.market_cap = None      # alphavantage
+        self.timestamp = datetime.datetime.now(datetime.timezone.utc)
+        self.market_cap = None      # nomics, alphavantage
 
         # Volume in 24 hours
-        self.volume = None          # alphavantage, kraken
+        self.volume = None          # nomics, kraken, alphavantage
 
         # Circulating supply
-        self.supply = None
+        self.supply = None          # nomics
 
-        self.all_time_high = None
-        self.all_time_low = None
+        self.all_time_high = None   # nomics
+
+        # self.all_time_low = None  # all time low doesn't really make sense
+
         self.high = None            # kraken
         self.low = None             # kraken
         self.open = None            # alphavantage
         self.close = None           # alphavantage
 
         # Price now X?
-        self.price = None           # coinbase
-        self.percent_return = None  # kraken?
-        self.dollar_return = None   # kraken?
+        self.price = None           # nomics, coinbase
+        self.percent_return = None  # kraken
+        self.dollar_return = None   # kraken
 
     def call_coinbase(self):
         try:
@@ -35,6 +37,7 @@ class CurrencyData():
             response.raise_for_status()
             self.price = float(response.json()["data"]["amount"])
         except Exception as e:
+            print("Coinbase error")
             print(e)
 
     def call_alphavantage(self):
@@ -52,8 +55,8 @@ class CurrencyData():
                 return
 
             #The timestamp is for yesterday (Some of the cryptos are not yet updated - easier to pull data for day before)
-            timeStamp = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d')
-            data = data["Time Series (Digital Currency Daily)"][f"{datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d')}"]
+            timeStamp = datetime.datetime.strftime(datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(1), '%Y-%m-%d')
+            data = data["Time Series (Digital Currency Daily)"][f"{datetime.datetime.strftime(datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(1), '%Y-%m-%d')}"]
 
             self.open = float(data["1a. open (USD)"])
             self.close = float(data["4a. close (USD)"])
@@ -61,25 +64,46 @@ class CurrencyData():
             self.market_cap = float(data["6. market cap (USD)"])
         
         except Exception as e:
+            print("Alpha Vantage error")
             print(e)
             
     def call_kraken(self):
         try:
             headers = {"X-CW-API-Key": Config.API_KEYS["CRYPTOWATCH"]}
-            url = "https://api.cryptowat.ch/markets/kraken/{}usd/summary".format(currency)
+            url = "https://api.cryptowat.ch/markets/kraken/{}usd/summary".format(self.currency)
             response = requests.request("GET", url, headers=headers)
             response.raise_for_status()
+            json_result = response.json()
 
-            #Absolute change in crypto
-            self.dollar_return = float(response.json()["result"]["price"]["change"]["absolute"])
-
-            #Percent change in crypto
-            self.percent_return = float(response.json()["result"]["price"]["change"]["percent"])
-            
-            self.high = str(response.json()["result"]["price"]["high"])
-            self.low = str(response.json()["result"]["price"]["low"])
+            # note that this is summary for only one exchange
+            self.dollar_return = float(json_result["result"]["price"]["change"]["absolute"])
+            self.percent_return = float(json_result["result"]["price"]["change"]["percentage"])
+            self.high = float(json_result["result"]["price"]["high"])
+            self.low = float(json_result["result"]["price"]["low"])
 
         except Exception as e:
+            print("Kraken error")
+            print(e)
+
+    def call_nomics(self):
+        # note that this API can call multiple currencies at once
+        # so we might want to convert to static method to call all currencies at once later
+        try:
+            parameters = {"key": Config.API_KEYS["NOMICS"], "ids": self.currency, "interval": "1d"}
+            url = "https://api.nomics.com/v1/currencies/ticker"
+            response = requests.request("GET", url, parameters=parameters)
+            response.raise_for_status()
+
+            # list of one currency
+            json_result = response.json()[0]
+
+            self.price = json_result["price"]
+            self.market_cap = json_result["market_cap"]
+            self.supply = json_result["circulating_supply"]
+            self.all_time_high = json_result["high"]
+
+        except Exception as e:
+            print("Nomics error")
             print(e)
 
 def on_trigger(event, context):
@@ -95,25 +119,35 @@ def on_trigger(event, context):
     client = MongoClient("mongodb+srv://emilys:UIUCchatbot@cluster0-ar3em.mongodb.net/test?retryWrites=true&w=majority")
     db = client["cryptocurrencyChatbot"]
 
-    # Iterating the index 
-    # same as 'for i in range(len(currencies))' 
+    data_dict = {}
+
+    # Iterator over the APIs that only return data for one currency at a time
     for currency in currencies:
         currency_data = CurrencyData(currency)
 
-        currency_data.call_coinbase()
+        # currency_data.call_coinbase()
         currency_data.call_alphavantage()
-        # currency_data.call_kraken()
-        
-        # timestamp is 12/02/2019 @ 2:30pm (UTC) - why are we using this date?
-        timestamp = datetime.datetime.fromtimestamp(1575297000).isoformat()
+        currency_data.call_kraken()
 
-        # Why are values inserted as strings into the database?
+        data_dict[currency] = currency_data
+        
+    for currency, currency_data in data_dict.items():
+        # Use current time
+        timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
+
         myDict = {
-            "Timestamp": currency_data.timestamp, 
+            "timestamp": currency_data.timestamp, 
             "price": currency_data.price, 
-            "Absolute change": currency_data.change, 
-            "High": currency_data.high, 
-            "low": currency_data.low
+            "absolute change": currency_data.dollar_return, 
+            "percent change": currency_data.percent_return,
+            "high": currency_data.high, 
+            "low": currency_data.low,
+            "all time high": currency_data.all_time_high,
+            "supply": currency_data.supply,
+            "market cap": currency_data.market_cap,
+            "volume": currency_data.volume,
+            "open": currency_data.open,
+            "close": currency_data.close
         }
         mycol = db[currency]
         x = mycol.insert_one(myDict)
